468f86c199b89874304f64b458d19a2ddd770685
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-08-22 18:17:58 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form=document.querySelector("#create-task-form")
+  form.addEventListener("submit",function(e){
+    e.preventDefault();
+    createNewTask(e.target.new_task_description.value)
+    form.reset()
+  })
+})
+function createNewTask(task){
+  let li=document.createElement("li");
+  let btn=document.createElement("button")
+  btn.addEventListener("click",removeList)
+  btn.textContent="Remove"
+  li.textContent=` ${task}    `
+  li.appendChild(btn)
+  document.querySelector("#tasks").appendChild(li)
+}
+function removeList(e){
+  e.target.parentNode.remove()
+}
